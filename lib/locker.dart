440e2a6c22d72fd11013a382c183a9f15a9d0ec8
brent4444 <bertibc@students.nu-dasma.edu.ph>
2024-07-24 19:34:47 +0800

    Initial commit

--- lib/locker.dart
+++ lib/locker.dart
@@ -21,46 +21,44 @@ class _LockerState extends State<Locker> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Container(color: Colors.white),
-          _buildHeader(),
-          _buildChooseLockerSize(),
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            _buildHeader(),
+            _buildChooseLockerSize(),
+            Expanded(
+              child: _buildLockerSizeList(),
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: _buildBottomNavigationBar(),
     );
   }
 
   Widget _buildHeader() {
-    return Positioned(
-      top: 70.0,
-      left: 30.0,
-      child: RichText(
-        text: TextSpan(
-          children: <TextSpan>[
-            TextSpan(
-              text: 'Locker',
-              style: TextStyle(
-                fontSize: 50.0,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Inter',
-                color: Color(0xFF002365),
-                decorationColor: Color(0xFF002365),
-              ),
-            ),
-          ],
+    return Padding(
+      padding: const EdgeInsets.only(left: 30.0, top: 20.0),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Text(
+          'Locker',
+          style: TextStyle(
+            fontSize: 50.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Inter',
+            color: Color(0xFF002365),
+          ),
         ),
       ),
     );
   }
 
   Widget _buildChooseLockerSize() {
-    return Positioned(
-      top: 150.0,
-      left: 0.0,
-      right: 0.0,
+    return Padding(
+      padding: EdgeInsets.only(top: 20.0), // Adjust this value to move it further down
       child: Container(
+        width: double.infinity,
         padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
         decoration: BoxDecoration(
           color: Color(0xFFD5E0F5),
@@ -75,6 +73,61 @@ class _LockerState extends State<Locker> {
               color: Color(0xFF002365),
             ),
           ),
+        ),
+      ),
+    );
+  }
+
+
+  Widget _buildLockerSizeList() {
+    return Padding(
+      padding: EdgeInsets.only(top: 30.0), // Add top padding here
+      child: ListView(
+        padding: EdgeInsets.all(20.0),
+        children: [
+          _buildLockerSizeRectangle('Small', '13" x 16" x 18"\n2.16 cubic ft\n\$8/day'),
+          SizedBox(height: 35),
+          _buildLockerSizeRectangle('Medium', '13" x 16" x 36"\n4.33 cubic ft\n\$12/day'),
+          SizedBox(height: 45),
+          _buildLockerSizeRectangle('Large', '13" x 16" x 72"\n8.66 cubic ft\n\$16/day'),
+        ],
+      ),
+    );
+  }
+
+
+  Widget _buildLockerSizeRectangle(String size, String details) {
+    return Container(
+      width: double.infinity,
+      height: 100.0, // Reduced height from 80.0 to 70.0
+      decoration: BoxDecoration(
+        color: Color(0xFF002365),
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              size,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0, // Slightly reduced font size
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Inter',
+              ),
+            ),
+            ...details.split('\n').map((line) => Text(
+              line,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 12.0, // Slightly reduced font size
+                fontFamily: 'Inter',
+              ),
+            )).toList(),
+          ],
         ),
       ),
     );
